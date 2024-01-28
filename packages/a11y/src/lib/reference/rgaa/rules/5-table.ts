e5ac82add3a85717b3a34a455ae263e29fb81969
Louis Fredice NJAKO MOLOM <131726886+luifr10@users.noreply.github.com>
2024-01-28 18:36:01 +0100

    feat(a11y): add rgaa criteria 5.6 to 5.8, #451

--- packages/a11y/src/lib/reference/rgaa/rules/5-table.ts
+++ packages/a11y/src/lib/reference/rgaa/rules/5-table.ts
@@ -1,5 +1,5 @@
-import {AutoCheckA11yRule, ManualCheckA11yRule} from "../../../model";
-import {AttributeChecker, ByTagQuery, CompliantAttributesQuery, OrQuery} from "../../../query";
+import { AutoCheckA11yRule, ManualCheckA11yRule } from "../../../model";
+import { AttributeChecker, ByTagQuery, CompliantAttributesQuery, OrQuery } from "../../../query";
 
 
 export default [
@@ -119,6 +119,230 @@ export default [
             "aria-labelledby"
         ],
         help: "adapt one of these attributes to be revelant"
+    }),
+    ManualCheckA11yRule.from({
+        criterion: "5.6",
+        wcag: "1.3.1 A",
+        id: "5.6.1",
+        elementType: "table",
+        query: new ByTagQuery([
+            "table:not([role=presentation])",
+            "[role=table]"
+        ]),
+        description: "table column header must be set properly",
+        help: "each table column header must be set with tag <th> or role=\"columnheader\""
+    }),
+    ManualCheckA11yRule.from({
+        criterion: "5.6",
+        wcag: "1.3.1 A",
+        id: "5.6.2",
+        elementType: "table",
+        query: new ByTagQuery([
+            "table:not([role=presentation])",
+            "[role=table]"
+        ]),
+        description: "table line header must be set properly",
+        help: "each table line header must be set with tag <th> or role=\"rowheader\""
+    }),
+    ManualCheckA11yRule.from({
+        criterion: "5.6",
+        wcag: "1.3.1 A",
+        id: "5.6.3",
+        elementType: "table",
+        query: new ByTagQuery([
+            "table:not([role=presentation])",
+            "[role=table]"
+        ]),
+        description: "each header does not apply to the entire row or column must be set properly"
+    }),
+    ManualCheckA11yRule.from({
+        criterion: "5.6",
+        wcag: "1.3.1 A",
+        id: "5.6.4",
+        elementType: "table",
+        query: new ByTagQuery([
+            "table:not([role=presentation])",
+            "[role=table]"
+        ]),
+        description: "each cell associated with several headers structured using a <th> or <td> tag"
+    }),
+    AutoCheckA11yRule.from({
+        criterion: "5.7",
+        wcag: "1.3.1 A",
+        id: "5.7.1",
+        elementType: "table",
+        query: new CompliantAttributesQuery(
+            new ByTagQuery([
+                "table:not([role=presentation]) tr:first-child th",
+                "[role=table] tr:first-child th"
+            ]),
+            [
+                AttributeChecker.notUniqueId(),
+                AttributeChecker.emptyAttribute("scope"),
+                AttributeChecker.notEquals("role", ["rowheader", "columnheader"])
+            ]
+        ),
+        description: "Table column header attributes",
+        help: "Correctly set table column header attributes"
+    }),
+    AutoCheckA11yRule.from({
+        criterion: "5.7",
+        wcag: "1.3.1 A",
+        id: "5.7.1",
+        elementType: "table",
+        query: new CompliantAttributesQuery(
+            new ByTagQuery([
+                "table:not([role=presentation]) tr:not(:first-child) th:first-child",
+                "[role=table] tr:not(:first-child) th:first-child"
+            ]),
+            [
+                AttributeChecker.notUniqueId(),
+                AttributeChecker.emptyAttribute("scope"),
+                AttributeChecker.notEquals("role", ["rowheader", "columnheader"])
+            ]
+        ),
+        description: "Table row header attributes",
+        help: "Correctly set table row header attributes"
+    }),
+    AutoCheckA11yRule.from({
+        criterion: "5.7",
+        wcag: "1.3.1 A",
+        id: "5.7.2",
+        elementType: "table",
+        query: new CompliantAttributesQuery(
+            new ByTagQuery([
+                "table:not([role=presentation]) tr:first-child th",
+                "[role=table] tr:first-child th"
+            ]),
+            [
+                AttributeChecker.notEmptyAttribute("scope"),
+                AttributeChecker.notEquals("scope", ["col"])
+            ]
+        ),
+        description: "Table column header scope attribute",
+        help: "Set table column header scope attribute to col"
+    }),
+    AutoCheckA11yRule.from({
+        criterion: "5.7",
+        wcag: "1.3.1 A",
+        id: "5.7.2",
+        elementType: "table",
+        query: new CompliantAttributesQuery(
+            new ByTagQuery([
+                "table:not([role=presentation]) tr:not(:first-child) th:first-child",
+                "[role=table] tr:not(:first-child) th:first-child"
+            ]),
+            [
+                AttributeChecker.notEmptyAttribute("scope"),
+                AttributeChecker.notEquals("scope", ["row"])
+            ]
+        ),
+        description: "Table row header scope attribute",
+        help: "Set table column header scope attribute to row"
+    }),
+    AutoCheckA11yRule.from({
+        criterion: "5.7",
+        wcag: "1.3.1 A",
+        id: "5.7.3",
+        elementType: "table",
+        query: new OrQuery([
+            new CompliantAttributesQuery(
+                new ByTagQuery([
+                    "table:not([role=presentation]) tr:not(:first-child) th:not(:first-child)"
+                ]),
+                [
+                    AttributeChecker.notUniqueId()
+                ]
+            ),
+            new CompliantAttributesQuery(
+                new ByTagQuery([
+                    "table:not([role=presentation]) tr:not(:first-child) th:not(:first-child)"
+                ]),
+                [
+                    AttributeChecker.notEmptyAttribute("scope")
+                ]
+            ),
+            new CompliantAttributesQuery(
+                new ByTagQuery([
+                    "table:not([role=presentation]) tr:not(:first-child) th:not(:first-child)"
+                ]),
+                [
+                    AttributeChecker.equals("role", ["rowheader", "columnheader"])
+                ]
+            )
+        ]),
+        description: "each header does not apply to the entire row or column must be set properly"
+    }),
+    ManualCheckA11yRule.from({
+        criterion: "5.7",
+        wcag: "1.3.1 A",
+        id: "5.7.4",
+        elementType: "table",
+        query: new ByTagQuery([
+            "table:not([role=presentation])",
+            "[role=table]"
+        ]),
+        description: "Each cell associated with several headers, the header attribute must be filled in correctly.",
+        help: "Each cell associated with several headers, the header attribute must contain the list of ids referring to the target headers."
+    }),
+    AutoCheckA11yRule.from({
+        criterion: "5.7",
+        wcag: "1.3.1 A",
+        id: "5.7.5",
+        elementType: "table",
+        query: new CompliantAttributesQuery(
+            new ByTagQuery([
+                "table:not([role=presentation]) tr:first-child th",
+                "[role=table] tr:first-child th"
+            ]),
+            [
+                AttributeChecker.notEmptyAttribute("role"),
+                AttributeChecker.notEquals("role", ["columnheader"])
+            ]
+        ),
+        description: "Table column header role attribute",
+        help: "Set table column header role attribute to 'columnheader'"
+    }),
+    AutoCheckA11yRule.from({
+        criterion: "5.7",
+        wcag: "1.3.1 A",
+        id: "5.7.5",
+        elementType: "table",
+        query: new CompliantAttributesQuery(
+            new ByTagQuery([
+                "table:not([role=presentation]) tr:not(:first-child) th:first-child",
+                "[role=table] tr:not(:first-child) th:first-child"
+            ]),
+            [
+                AttributeChecker.notEmptyAttribute("role"),
+                AttributeChecker.notEquals("role", ["rowheader"])
+            ]
+        ),
+        description: "Table row header role attribute",
+        help: "Set table row header role attribute to 'rowheader'"
+    }),
+    AutoCheckA11yRule.from({
+        criterion: "5.8",
+        wcag: "1.3.1 A",
+        id: "5.8.1",
+        elementType: "table",
+        query: new ByTagQuery([
+            "table[role=presentation][summary]",
+            "table[role=presentation]:has(caption)",
+            "table[role=presentation]:has(thead)",
+            "table[role=presentation]:has(th)",
+            "table[role=presentation]:has(tfoot)",
+            "table[role=presentation] :has(> [role=rowheader])",
+            "table[role=presentation] :has(> [role=columnheader])",
+            "table[role=presentation] td[scope]",
+            "table[role=presentation] td[headers]",
+            "table[role=presentation] td[axis]",
+        ]),
+        description: "table with presentation role must have revelant content",
+        attributes: [
+            "child tag <td>"
+        ],
+        help: "adapt these <td> children content to be relevant"
     })
 ];
 
--- packages/a11y/tests/checker/5-table/table-checker.spec.ts
+++ packages/a11y/tests/checker/5-table/table-checker.spec.ts
@@ -208,4 +208,196 @@ describe("5-table", () => {
             }
         });
     });
+
+    it("5.6 should return results", async () => {
+        await initA11yOnPage("table.html");
+        const { result, summary } = await validateA11y(page.url(), ["5.6"]);
+        expect(result.status).toEqual(A11yResultStatus.MANUAL);
+        const [_5_6_1, _5_6_2, _5_6_3, _5_6_4] = result.ruleResults;
+        const allTablesOfContent = [
+            "table[data-testid=table]",
+            "table[data-testid=table-with-summary]",
+            "table[data-testid=table-with-caption]",
+            "table[data-testid=table-with-caption-empty]",
+            "table[data-testid=table-with-aria-describedby-not-existing]",
+            "table[data-testid=table-with-aria-describedby-existing]",
+            "table[data-testid=table-with-aria-labelledby-existing]",
+            "table[data-testid=table-with-aria-labelledby-not-existing]",
+            "table[data-testid=table-with-aria-label-empty]",
+            "table[data-testid=table-with-aria-label]",
+            "div[data-testid=div-with-role-table]",
+            "div[data-testid=div-with-role-table-and-caption]",
+            "div[data-testid=div-with-role-table-and-caption-empty]",
+            "div[data-testid=div-with-aria-describedby-not-existing]",
+            "div[data-testid=div-with-aria-describedby-existing]",
+            "div[data-testid=div-with-aria-labelledby-not-existing]",
+            "div[data-testid=div-with-aria-labelledby-existing]",
+            "div[data-testid=div-with-aria-label-empty]",
+            "div[data-testid=div-with-aria-label]",
+        ];
+        checkTest(_5_6_1, "5.6.1",
+            A11yResultStatus.MANUAL,
+            [],
+            allTablesOfContent
+        );
+        checkTest(_5_6_2, "5.6.2",
+            A11yResultStatus.MANUAL,
+            [],
+            allTablesOfContent
+        );
+        checkTest(_5_6_3, "5.6.3",
+            A11yResultStatus.MANUAL,
+            [],
+            allTablesOfContent
+        );
+        checkTest(_5_6_4, "5.6.4",
+            A11yResultStatus.MANUAL,
+            [],
+            allTablesOfContent
+        );
+        expect(summary).toEqual({
+            status: A11yResultStatus.MANUAL,
+            criteria: {
+                "5.6": {
+                    status: A11yResultStatus.MANUAL,
+                    tests: expect.objectContaining({
+                        "5.6.1": {
+                            status: A11yResultStatus.MANUAL
+                        },
+                        "5.6.2": {
+                            status: A11yResultStatus.MANUAL
+                        },
+                        "5.6.3": {
+                            status: A11yResultStatus.MANUAL
+                        },
+                        "5.6.4": {
+                            status: A11yResultStatus.MANUAL
+                        }
+                    })
+                }
+            }
+        });
+    });
+
+    it("5.7 should return results", async () => {
+        await initA11yOnPage("table-with-header.html");
+        const { result, summary } = await validateA11y(page.url(), ["5.7"]);
+        expect(result.status).toEqual(A11yResultStatus.ERROR);
+        const [_5_7_1_COLUMN, _5_7_1_ROW, _5_7_2_COLUMN, _5_7_2_ROW, _5_7_3, _5_7_5_COLUMN, _5_7_5_ROW, _5_7_4] = result.ruleResults;
+        checkTest(_5_7_1_COLUMN, "5.7.1",
+            A11yResultStatus.ERROR,
+            [
+                "table[data-testid=first-table] > tbody > tr:nth-of-type(1) > th:nth-of-type(2)",
+                "th#an-id"
+            ]
+        );
+        checkTest(_5_7_1_ROW, "5.7.1",
+            A11yResultStatus.ERROR,
+            [
+                "th#an-id",
+                "table[data-testid=first-table] > tbody > tr:nth-of-type(7) > th:nth-of-type(1)"
+            ]
+        );
+        checkTest(_5_7_2_COLUMN, "5.7.2",
+            A11yResultStatus.ERROR,
+            [
+                "table[data-testid=first-table] > tbody > tr:nth-of-type(1) > th:nth-of-type(5)"
+            ]
+        );
+        checkTest(_5_7_2_ROW, "5.7.2",
+            A11yResultStatus.ERROR,
+            [
+                "table[data-testid=first-table] > tbody > tr:nth-of-type(8) > th:nth-of-type(1)"
+            ]
+        );
+        checkTest(_5_7_3, "5.7.3",
+            A11yResultStatus.ERROR,
+            [
+                "table[data-testid=first-table] > tbody > tr:nth-of-type(2) > th:nth-of-type(2)",
+                "th#an-id",
+                "table[data-testid=first-table] > tbody > tr:nth-of-type(4) > th:nth-of-type(2)",
+                "table[data-testid=first-table] > tbody > tr:nth-of-type(5) > th:nth-of-type(2)",
+                "table[data-testid=first-table] > tbody > tr:nth-of-type(8) > th:nth-of-type(2)"
+            ]
+        );
+        checkTest(_5_7_4, "5.7.4",
+            A11yResultStatus.MANUAL,
+            [],
+            [
+                "table[data-testid=first-table]"
+            ]
+        );
+        checkTest(_5_7_5_COLUMN, "5.7.5",
+            A11yResultStatus.ERROR,
+            [
+                "table[data-testid=first-table] > tbody > tr:nth-of-type(1) > th:nth-of-type(3)"
+            ]
+        );
+        checkTest(_5_7_5_ROW, "5.7.5",
+            A11yResultStatus.ERROR,
+            [
+                "table[data-testid=first-table] > tbody > tr:nth-of-type(6) > th:nth-of-type(1)"
+            ]
+        );
+        expect(summary).toEqual({
+            status: A11yResultStatus.ERROR,
+            criteria: {
+                "5.7": {
+                    status: A11yResultStatus.ERROR,
+                    tests: expect.objectContaining({
+                        "5.7.1": {
+                            status: A11yResultStatus.ERROR
+                        },
+                        "5.7.2": {
+                            status: A11yResultStatus.ERROR
+                        },
+                        "5.7.3": {
+                            status: A11yResultStatus.ERROR
+                        },
+                        "5.7.4": {
+                            status: A11yResultStatus.MANUAL
+                        },
+                        "5.7.5": {
+                            status: A11yResultStatus.ERROR
+                        }
+                    })
+                }
+            }
+        });
+    });
+
+    it("5.8 should return results", async () => {
+        await initA11yOnPage("table-presentation.html");
+        const { result, summary } = await validateA11y(page.url(), ["5.8"]);
+        expect(result.status).toEqual(A11yResultStatus.ERROR);
+        const [_5_8_1] = result.ruleResults;
+        checkTest(_5_8_1, "5.8.1",
+            A11yResultStatus.ERROR,
+            [
+                "table[data-testid=table-with-summary]",
+                "table[data-testid=table-with-caption]",
+                "table[data-testid=table-with-thead]",
+                "table[data-testid=table-with-th]",
+                "table[data-testid=table-with-tfoot]",
+                "table[data-testid=table-with-rowheader] > tbody > tr:nth-of-type(1)",
+                "table[data-testid=table-with-columnheader] > tbody > tr:nth-of-type(1)",
+                "table[data-testid=table-with-scope] > tbody > tr:nth-of-type(1) > td:nth-of-type(1)",
+                "table[data-testid=table-with-headers] > tbody > tr:nth-of-type(1) > td:nth-of-type(1)",
+                "table[data-testid=table-with-axis] > tbody > tr:nth-of-type(1) > td:nth-of-type(1)"
+            ]
+        );
+        expect(summary).toEqual({
+            status: A11yResultStatus.ERROR,
+            criteria: {
+                "5.8": {
+                    status: A11yResultStatus.ERROR,
+                    tests: expect.objectContaining({
+                        "5.8.1": {
+                            status: A11yResultStatus.ERROR
+                        }
+                    })
+                }
+            }
+        });
+    });
 });
